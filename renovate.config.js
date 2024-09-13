0cd620d48a72e4f1d77ed1182d8468d115ca82b2
David Blane <32327139+dblane-digicatapult@users.noreply.github.com>
2024-09-13 19:24:55 +0100

    readme-generator (#138)
    * Adds labels and makes use of the readme-generator to update the README when we change values in a chart
    
    * fix regex

--- renovate.config.js
+++ renovate.config.js
@@ -1,66 +1,78 @@
 module.exports = (config = {}) => {
-  const isSelfHosted = process.env.RENOVATE_SELF_HOSTED === 'true';
+  const isSelfHosted = process.env.RENOVATE_SELF_HOSTED === "true";
 
   if (!isSelfHosted) {
-    console.log('Renovate is disabled when running via GitHub App.');
+    console.log("Renovate is disabled when running via GitHub App.");
     return {
       enabled: false,
       onboarding: false,
     };
   }
 
-  console.log('Renovate is running in self-hosted mode.');
+  console.log("Renovate is running in self-hosted mode.");
 
   return {
-    $schema: 'https://docs.renovatebot.com/renovate-schema.json',
+    $schema: "https://docs.renovatebot.com/renovate-schema.json",
     onboarding: false,
     requireConfig: false,
-    allowedPostUpgradeCommands: ['scripts/bump-chart-version.sh'],
+    allowedPostUpgradeCommands: ["scripts/bump-chart-version.sh"],
     prHourlyLimit: 20,
     prConcurrentLimit: 20,
     customManagers: [
       {
-        customType: 'regex',
-        datasourceTemplate: 'docker',
-        fileMatch: ['(^|/)Chart\\.yaml$'],
+        customType: "regex",
+        datasourceTemplate: "docker",
+        fileMatch: ["(^|/)Chart\\.yaml$"],
         matchStrings: [
-          '#\\s*renovate: image=(?<imageName>.*?)\\s+appVersion:\\s*[\'"]?(?<currentValue>[\\w+\\.\\-]*)',
+          '#\\s*renovate: image=(?<imageName>.*?)\\s+appVersion:\\s*[\"]?(?<currentValue>[\\w+\\.\\-]*)',
         ],
-        depNameTemplate: 'docker.io/{{{imageName}}}',
+        depNameTemplate: "docker.io/{{{imageName}}}",
       },
     ],
     packageRules: [
       {
-        description: 'Group updates per dependency across Chart.yaml and values.yaml',
-        matchManagers: ['helm-values', 'regex'],
-        groupName: '{{{depName}}}',
+        description:
+          "Group updates per dependency across Chart.yaml and values.yaml",
+        matchManagers: ["helm-values", "regex"],
+        groupName: "{{{depName}}}",
+        labels: ["dependencies", "helm"],
       },
       {
-        description: 'Always bump chart version by a patch when updating values files.',
-        matchManagers: ['helm-values', 'regex'],
+        description:
+          "Always bump chart version by a patch when updating values files.",
+        matchManagers: ["helm-values", "regex"],
         postUpgradeTasks: {
           commands: ["scripts/bump-chart-version.sh '{{{parentDir}}}'"],
-          fileFilters: ['**/Chart.yaml'],
-          executionMode: 'branch',
+          fileFilters: ["**/Chart.yaml"],
+          executionMode: "branch",
         },
       },
       {
-        matchManagers: ['helm-values', 'regex'],
-        matchUpdateTypes: ['patch'],
+        matchManagers: ["helm-values", "regex"],
+        matchUpdateTypes: ["patch"],
         automerge: true,
-        automergeType: 'pr',
-        labels: ['automerge'],
+        automergeType: "pr",
+        labels: ["dependencies", "helm"],
       },
       {
-        matchManagers: ['helm-values', 'regex'],
-        matchUpdateTypes: ['minor', 'major'],
+        matchManagers: ["helm-values", "regex"],
+        matchUpdateTypes: ["minor", "major"],
+        automerge: false,
+        labels: ["dependencies", "helm"],
+      },
+      {
+        matchManagers: ["helmv3"],
+        groupName: "Chart Dependency Updates for {{parentDir}}",
+        separateMajorMinor: true,
+        labels: ["dependencies", "helm"],
         automerge: false,
       },
       {
-        matchManagers: ['github-actions'],
-        matchUpdateTypes: ['patch'],
-        automerge: true,
-        labels: ['automerge'],
+        matchManagers: ["github-actions"],
+        groupName: "{{{depName}}} GitHub Action Updates",
+        separateMajorMinor: true,
+        labels: ["dependencies", "github-actions"],
+        automerge: false,
       },
     ],
   };
